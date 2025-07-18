be9f0f05c4fa9370c3cc8ef068a150554f826dce
maks1008 <maks.solomakhin@gmail.com>
2025-07-19 04:43:58 +1200

    final bug fixes relating to glazing and ragebaiting

--- botActions/ragebait/ragebait.py
+++ botActions/ragebait/ragebait.py
@@ -2,7 +2,9 @@ import random
 
 def ragebait():
     """returns a random ragebait message (selected from ragebaitMessages.txt)"""
-    with open('botActions/ragebait/ragebaitMessages.txt') as ragebait_file:
+    with open('botActions/ragebait/ragebaitMessages.txt', encoding='utf-8') as ragebait_file:
         ragebait_list = [line.strip() for line in ragebait_file]
     random.shuffle(ragebait_list)
     return ragebait_list[0]
+
+print(ragebait())
