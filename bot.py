0ec243a841cb3f7f4df20d00bde2423538cfd1ee
maks1008 <maks.solomakhin@gmail.com>
2025-07-19 03:52:51 +1200

    hooked glazing and ragebaiting up to bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,25 @@
 import discord
+import random
+from botActions.ragebait.ragebait import ragebait
+from botActions.glaze.glaze import glazing
+
+#if random value == one of these, the respective response is triggered
+glaze_value = 1
+ragebait_value = 2
 
 class Client(discord.Client): #necessary for the bot to run
     async def on_ready(self): #this function executes when the bot first goes online
         print("{} is back!".format(self.user))
+    
+    async def on_message(self, message): #when a message is sent
+        action_value = random.randrange(1,61) # generates a number from 1 to 60
+        if message.author == self.user:
+            return
+        if action_value == glaze_value:
+            await message.channel.send(glazing())
+        elif action_value == ragebait_value:
+            await message.channel.send(ragebait())
+        
     
 intents = discord.Intents.default() #enables intents
 intents.message_content = True
