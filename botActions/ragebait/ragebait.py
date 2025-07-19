b80ad0c6ad414a70401a96747dd452b107d76cdd
maks1008 <maks.solomakhin@gmail.com>
2025-07-19 14:40:33 +1200

    slight error fix in ragebait.py

--- botActions/ragebait/ragebait.py
+++ botActions/ragebait/ragebait.py
@@ -5,6 +5,4 @@ def ragebait():
     with open('botActions/ragebait/ragebaitMessages.txt', encoding='utf-8') as ragebait_file:
         ragebait_list = [line.strip() for line in ragebait_file]
     random.shuffle(ragebait_list)
-    return ragebait_list
-
-print(ragebait())
+    return ragebait_list[0]
