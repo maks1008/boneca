ef4e45f39ac843dbf29b0eaaeb86ba692491d55e
maks1008 <maks.solomakhin@gmail.com>
2025-07-19 14:38:37 +1200

    added speech bubble gifs to ragebait

--- botActions/ragebait/ragebait.py
+++ botActions/ragebait/ragebait.py
@@ -5,4 +5,6 @@ def ragebait():
     with open('botActions/ragebait/ragebaitMessages.txt', encoding='utf-8') as ragebait_file:
         ragebait_list = [line.strip() for line in ragebait_file]
     random.shuffle(ragebait_list)
-    return ragebait_list[0]
+    return ragebait_list
+
+print(ragebait())
