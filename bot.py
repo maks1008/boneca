7e415ab0a977d55b812d6271ac3124798df4d696
maks1008 <maks.solomakhin@gmail.com>
2025-07-20 14:02:26 +1200

    introduced /not me, refactored bot.py, looked into the GIF issue

--- bot.py
+++ bot.py
@@ -5,12 +5,14 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-MESSAGE_RESPONSE_FREQUENCY = 4
+MESSAGE_RESPONSE_FREQUENCY = 1
 RBBT_SERVER_ID = discord.Object(id=1368129546578300978)
 
 class Client(commands.Bot):
     async def on_ready(self):
         print("{} is back!".format(self.user))
+        admin_utils.unpack()
+        admin_utils.unpack_do_not_target()
 
         try:
             synced = await self.tree.sync(guild=RBBT_SERVER_ID)
@@ -18,11 +20,15 @@ class Client(commands.Bot):
 
         except Exception as e:
             print("ERROR: {}".format(e))
-
     
     async def on_message(self, message):
-        #IGNORES ITS OWN MESSAGES
+
+        #IGNORES THE FOLLOWING MESSAGES
         if message.author == self.user:
+            return
+        if admin_utils.dnt_user(str(message.author.id)):
+            return
+        if not admin_utils.accepted_channel(str(message.channel.mention)):
             return
         
         #RUNS TRIGGER WORD DETECTOR
@@ -30,13 +36,13 @@ class Client(commands.Bot):
         if trigger is not False:
             await message.channel.send(messageReactions.trigger_message(trigger))
             return
-
+        
         #GENERATES A MESSAGE RESPONSE (glazing/ragebait)
         action_value = random.randrange(1, 2 * MESSAGE_RESPONSE_FREQUENCY + 1)
-        if action_value == 1 and admin_utils.accepted_channel(str(message.channel.mention)):
+        if action_value == 1:
             await message.channel.send(messageReactions.message_response("GLAZING"))
             return
-        elif action_value == 2 and admin_utils.accepted_channel(str(message.channel.mention)):
+        elif action_value == 2:
             await message.channel.send(messageReactions.message_response("RAGEBAITING"))
             return
         
@@ -44,7 +50,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = Client(command_prefix="!", intents=intents) #command prefix is arbitrary, discord enforces slash commands
 
-@client.tree.command(name="introduce", description="introduces boneca into this channel", guild=RBBT_SERVER_ID)
+@client.tree.command(name="introduce", description="Introduce Boneca to this channel", guild=RBBT_SERVER_ID)
 async def introduce_boneca(interaction: discord.Interaction):
     channel_id = str(interaction.channel.mention)
     if admin_utils.accepted_channel(channel_id):
@@ -53,7 +59,7 @@ async def introduce_boneca(interaction: discord.Interaction):
         admin_utils.introduce(channel_id)
         await interaction.response.send_message("You have given me permission to interact with {}!".format(interaction.channel.mention))
 
-@client.tree.command(name="banish", description="removes boneca from this channel", guild=RBBT_SERVER_ID)
+@client.tree.command(name="banish", description="Remove Boneca from this channel", guild=RBBT_SERVER_ID)
 async def banish_boneca(interaction: discord.Interaction):
     channel_id = str(interaction.channel.mention)
     if admin_utils.accepted_channel(channel_id):
@@ -62,15 +68,13 @@ async def banish_boneca(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("I can't talk here anyway :joy_cat::pray:")
 
-@client.tree.command(name="report", description="flag boneca's last message as innapropriate", guild=RBBT_SERVER_ID)
+@client.tree.command(name="report", description="Flag Boneca's last message as innapropriate", guild=RBBT_SERVER_ID)
 async def report_boneca(interaction: discord.Integration):
     channel = interaction.channel
     async for message in channel.history(limit=20):
         if message.author == client.user:
-            
             datkid = await client.fetch_user(572732144195993609)
             await datkid.send("**{} USED /REPORT:** {}".format(interaction.user.name, message.content))
-
             report_status = admin_utils.report(message.content)
             await message.delete()
             if report_status:
@@ -78,8 +82,17 @@ async def report_boneca(interaction: discord.Integration):
             else:
                 await interaction.response.send_message("I'm sorry :worried: I took that too far... I have asked the devs to review this one ASAP.")
             return
-        
     await interaction.channel.send("I can't figure out what I did wrong :disappointed: Please get in touch with @datkid10021 ASAP.")
+
+@client.tree.command(name="notme", description="Toggle Boneca's permissions to interact with you", guild=RBBT_SERVER_ID)
+async def notme_boneca(interaction: discord.Integration):
+    user = str(interaction.user.id)
+    if admin_utils.dnt_user(user):
+        admin_utils.not_me2(user)
+        await interaction.response.send_message("You've been removed from Boneca's safe list. Boneca will keep an eye out on you!")
+    else:
+        admin_utils.not_me1(user)
+        await interaction.response.send_message("You've been added to Boneca's safe list. Boneca will never target you.")
 
 
 
--- commands/administrativeCommands/admin_utils.py
+++ commands/administrativeCommands/admin_utils.py
@@ -1,6 +1,9 @@
 server_permissions_file = 'commands/administrativeCommands/bonecaServerPermissions.txt'
 accepted_channel_id = set()
 
+do_not_target_file = 'commands/administrativeCommands/doNotTarget.txt'
+do_not_target_set = set()
+
 glaze_messages_file = 'botActions/glazeMessages.txt'
 ragebait_messages_file = 'botActions/ragebaitMessages.txt'
 quarantined_messages_file = 'commands/administrativeCommands/quarantinedMessages.txt'
@@ -13,11 +16,23 @@ def unpack():
     for i in channelID_list:
         accepted_channel_id.add(i)
 
+def unpack_do_not_target():
+    """converts all channel ids from doNotTarget.txt into the accepted_channel_id set"""
+    with open(do_not_target_file, encoding='utf-8') as dnt_file:
+        dnt_list = [line.strip() for line in dnt_file]
+    for i in dnt_list:
+        do_not_target_set.add(i) 
+
 def update_server_permissions():
     """updates bonecaServerPermissions.txt to whats currently in accepted_channel_id"""
     with open(server_permissions_file, 'w', encoding='utf-8') as channelID_file:
         for i in accepted_channel_id:
             channelID_file.write("{}\n".format(i))
+
+def update_doNotTargetTXT():
+    with open(do_not_target_file, 'w', encoding='utf-8') as dnt_file:
+        for i in do_not_target_set:
+            dnt_file.write("{}\n".format(i))
 
 def introduce(channel_id):
     """adds channel_id to accepted_channel_id and bonecaServerPermissions.txt"""
@@ -32,6 +47,10 @@ def banish(channel_id):
 def accepted_channel(channel_id):
     """checks if channel_id is an accepted channel"""
     return channel_id in accepted_channel_id
+
+def dnt_user(user):
+    """checks if user is in the do_not_target_set"""
+    return user in do_not_target_set
 
 def report(message):
     """#1 - quarantines message 
@@ -58,3 +77,13 @@ def report(message):
         return True
     
     return False
+
+def not_me1(user):
+    """adds user to do_not_target_set and do_not_target_file"""
+    do_not_target_set.add(user)
+    update_doNotTargetTXT()
+
+def not_me2(user):
+    """removes user from do_not_target_set and do_not_target_file"""
+    do_not_target_set.discard(user)
+    update_doNotTargetTXT()
